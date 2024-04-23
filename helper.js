29c1962c035394930d9cbd637bf0ef9113522526
Cong Huy <conghuy123zzz@gmail.com>
2024-04-23 11:33:08 +0700

    fix-helper

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 const base64url = str =>{
-  return btoa(str).replace(/\+/,'-').replace(/\//,'-').replace(/\=/,'')
+  return btoa(str).replace(/\+/,'-').replace(/\//,'_').replace(/\=/,'')
 }
 
 module.exports={base64url}
